1cec49552165f380ad01b9378b348075eb93464a
gmito5025-max <gmito5025@gmail.com>
2025-12-05 11:06:00 -0300

    Delete ESTA QUASE COMPLETO - TERMINANDO directory

--- ESTA QUASE COMPLETO - TERMINANDO/serviços/script-voltar.js
+++
@@ -1,8 +0,0 @@
-// ===== Botão de Voltar =====
-const botaoVoltar = document.querySelector('.back-button');
-
-if (botaoVoltar) {
-  botaoVoltar.addEventListener('click', () => {
-    window.location.href = 'escolha-serviços.html';
-  });
-}
